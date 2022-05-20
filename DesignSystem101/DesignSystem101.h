e537c647d019bcf5fa8f743f7c7f3ee6f5b9989e
Jessy Naus <jessyn@spotify.com>
2022-05-20 09:37:59 +0200

    Initial Commit

---
+++ DesignSystem101/DesignSystem101.h
@@ -0,0 +1,18 @@
+//
+//  DesignSystem101.h
+//  DesignSystem101
+//
+//  Created by Jessy Naus on 2022-05-20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DesignSystem101.
+FOUNDATION_EXPORT double DesignSystem101VersionNumber;
+
+//! Project version string for DesignSystem101.
+FOUNDATION_EXPORT const unsigned char DesignSystem101VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignSystem101/PublicHeader.h>
+
+
